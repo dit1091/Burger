269eb0db24e1e3c60bc288bc12a03bcba50ddda2
Dannette Tejeda <dannettetejeda@gmail.com>
2020-11-04 16:24:05 -0500

    server update

--- server.js
+++ server.js
@@ -16,3 +16,4 @@ const PORT = process.env.PORT || 8080;
 app.listen(PORT, function() {
   // Log (server-side) when our server has started
   console.log("Server listening on: http://localhost:" + PORT);
+});
